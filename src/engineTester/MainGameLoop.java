8f3ab13294ee8d51b0e0e03361deb2a3b1d5954c
Sabberkopf <78373419+Sabberkopf@users.noreply.github.com>
2021-10-27 08:25:44 +0200

    LoadOBJ

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -8,6 +8,7 @@ import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -20,89 +21,9 @@ public class MainGameLoop {
         StaticShader staticShader = new StaticShader();
         Renderer renderer = new Renderer(staticShader);
 
+        RawModel model = OBJLoader.loadObjModel("stall",loader);
 
-
-        float[] vertices = {
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,-0.5f,0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                0.5f,0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                -0.5f,-0.5f,0.5f,
-                -0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,-0.5f,0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f
-
-        };
-
-        float[] textureCorords = {
-
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-
-
-        };
-
-        int[] indices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-
-        };
-        RawModel model = loader.loadToVAO(vertices,textureCorords,indices);
-
-        ModelTexture texture = new ModelTexture(loader.loadTexture("grass"));
+        ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
 
         TexturedModel texturedModel = new TexturedModel(model,texture);
 
@@ -111,8 +32,7 @@ public class MainGameLoop {
         Camera camera = new Camera();
 
         while (!Display.isCloseRequested()){
-            entity.increasePosition(0,0,0);
-            entity.increaseRotation(0.01f,0.01f,0);
+            entity.increaseRotation(0,0.01f,0);
             camera.move();
             renderer.prepare();
             staticShader.start();
--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -25,6 +25,12 @@ public class Camera {
         if(Keyboard.isKeyDown(Keyboard.KEY_S)){
             position.z+=0.02f;
         }
+        if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
+            position.y+=0.02f;
+        }
+        if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)){
+            position.y-=0.02f;
+        }
     }
 
     public Vector3f getPosition() {
